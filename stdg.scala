1b79eeaab428c5ac53dbb5e16f80f0373b2e4c11
djjansz <djjansz@gmail.com>
2021-03-28 00:55:26 -0400

    Added examples from chapter 5.

--- stdg.scala
+++ stdg.scala
@@ -137,3 +137,170 @@ spark.range(15).toDF().show()
 // declare a column to be an IntegerType column
 import org.apache.spark.sql.types._
 val b = IntegerType
+// create a dataframe from a json file
+val df = spark.read.format("json").load("/user/sjf/data/flight_data/json/2015-summary.json")
+// show the schema
+df.printSchema()
+// show the schema right on import - shows it horizontally instead of vertically
+spark.read.format("json").load("/user/sjf/data/flight_data/json/2015-summary.json").schema
+// create and inforce a schema upon the data
+import org.apache.spark.sql.types.{StructField, StructType, StringType, LongType}
+import org.apache.spark.sql.types.Metadata
+val myManualSchema = StructType(Array(
+  StructField("DEST_COUNTRY_NAME", StringType, true),
+  StructField("ORIGIN_COUNTRY_NAME", StringType, true),
+  StructField("count", LongType, false,
+    Metadata.fromJson("{\"hello\":\"world\"}"))
+))
+val df = spark.read.format("json").schema(myManualSchema).load("/user/sjf/data/flight_data/json/2015-summary.json")
+//creating a new column from scratch
+import org.apache.spark.sql.functions.{col, column}
+col("someColumnName")
+column("someColumnName")
+$"myColumn"
+//refer to a specific column
+df.col("count")
+// an expression - a transformation on one or more values of a record in a DataFrame
+(((col("someCol") + 5) * 200) - 6) < col("otherCol")
+// in Scala
+import org.apache.spark.sql.functions.expr
+expr("(((someCol + 5) * 200) - 6) < otherCol")
+//use the columns property to see all columns of the dataFrame
+spark.read.format("json").load("/user/sjf/data/flight_data/json/2015-summary.json").columns
+// see the first row of the DataFrame imported from the json file
+df.first()
+// creating a row manually
+import org.apache.spark.sql.Row
+val myRow = Row("Hello", null, 1, false)
+// you can't use the show() or collect() action on a row or column, instead you access and show data by position
+myRow(0)
+//coerce the third element into an integer
+myRow.getInt(2) 
+// create a dataFrame from a json file
+val df = spark.read.format("json").load("/user/sjf/data/flight_data/json/2015-summary.json")
+//register the dataFrame as a temporary view to be queried by SQL
+df.createOrReplaceTempView("dfTable")
+// manually create dataFrame from an RDD
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types.{StructField, StructType, StringType, LongType}
+val myManualSchema = new StructType(Array(
+  new StructField("some", StringType, true),
+  new StructField("col", StringType, true),
+  new StructField("names", LongType, false)))
+val myRows = Seq(Row("Hello", null, 1L))
+val myRDD = spark.sparkContext.parallelize(myRows)
+val myDf = spark.createDataFrame(myRDD, myManualSchema)
+myDf.show()
+// turn a sequence into a row of a dataFrame
+val myDF = Seq(("Hello", 2, 1L)).toDF("col1", "col2", "col3")
+myDF.show()
+// select a sepecific column of your dataFrame
+df.select("DEST_COUNTRY_NAME", "ORIGIN_COUNTRY_NAME").show(2)
+// referring to coumns in different ways
+import org.apache.spark.sql.functions.{expr, col, column}
+df.select($"DEST_COUNTRY_NAME").show(2)
+// rename the column to destination
+df.select(expr("DEST_COUNTRY_NAME AS destination")).show(2)
+// rename the column to destination and then back to DEST_COUNTRY_NAME
+df.select(expr("DEST_COUNTRY_NAME as destination").alias("DEST_COUNTRY_NAME")).show(2)
+// showing a column beside itself
+df.selectExpr("DEST_COUNTRY_NAME as newColumnName", "DEST_COUNTRY_NAME").show(2)
+// selectExpr is the shorthand for the popular select(expr())
+df.selectExpr("*", "(DEST_COUNTRY_NAME = ORIGIN_COUNTRY_NAME) as withinCountry").show(100)
+// in SQL this would be SELECT avg(count) as avg_count,count(distinct(DEST_COUNTRY_NAME)) as distinct_countries FROM dfTABLE LIMIT 2
+df.selectExpr("avg(count) as average_count", "count(distinct(DEST_COUNTRY_NAME)) as distinct_countries").show(2)
+// 1 as One using Spark literals
+import org.apache.spark.sql.functions.lit
+df.select(expr("*"), lit(1).as("One")).show(2)
+// 1 as One, with the withColumn method
+df.withColumn("One", lit(1)).show(2)
+// create a new column named withinCountry 
+df.withColumn("withinCountry", expr("ORIGIN_COUNTRY_NAME == DEST_COUNTRY_NAME")).show(100)
+// create a new column DEST_COUNTRY_NAME as Destination
+df.withColumn("Destination", expr("DEST_COUNTRY_NAME")).columns
+// rename DEST_COUNTRY_NAME to dest
+df.withColumnRenamed("DEST_COUNTRY_NAME", "dest").columns
+// in Scala
+import org.apache.spark.sql.functions.expr
+//create a long column name with reserved characters like spaces and dashes
+val dfWithLongColName = df.withColumn("This Long Column-Name",expr("ORIGIN_COUNTRY_NAME"))
+// handling long column names with backticks
+dfWithLongColName.selectExpr("`This Long Column-Name`","`This Long Column-Name` as `new col`").show(2)
+// create a view dfTableLong
+dfWithLongColName.createOrReplaceTempView("dfTableLong")
+// select just the one column and use the columns method to show that
+dfWithLongColName.select(col("This Long Column-Name")).columns
+//drop the ORIGIN_COUNTRY_NAME column
+df.drop("ORIGIN_COUNTRY_NAME").columns
+// drop two columns
+dfWithLongColName.drop("ORIGIN_COUNTRY_NAME", "DEST_COUNTRY_NAME").show()
+// convert an integer variable to long with the cast method
+df.withColumn("count2", col("count").cast("long")).schema
+// use the filter method to filter rows
+df.filter(col("count") < 2).show(20)
+// use the where method to filter
+df.where("count < 2").show(20)
+// chaining the where method is better than specifiying multiple AND filters
+df.where(col("count") < 2).where(col("ORIGIN_COUNTRY_NAME") =!= "Croatia").show(20)
+// in Scala
+df.select("ORIGIN_COUNTRY_NAME", "DEST_COUNTRY_NAME").distinct().count()
+// use the distinct method on a dataframe to extract the unique values
+df.select("ORIGIN_COUNTRY_NAME").distinct().count()
+// taking a sample of rows - this method has a 1% chance of picking a row and it picks five of the 256 rows
+val seed = 12345
+val withReplacement = false
+val fraction = 0.01
+df.sample(withReplacement, fraction, seed).show()
+// sample the dataFrame with a random split - often used to create training, validation and test dataFrames
+val dataFrames = df.randomSplit(Array(0.25, 0.75), seed)
+//show the row count of the smaller dataFrame
+dataFrames(0).count()
+//show that row count of the larger dataFrame
+dataFrames(1).count()
+// in Scala
+import org.apache.spark.sql.Row
+val schema = df.schema
+val newRows = Seq(
+  Row("New Country", "Other Country", 5L),
+  Row("New Country 2", "Other Country 3", 1L)
+)
+//create an array that is a flattened version of the table
+val parallelizedRows = spark.sparkContext.parallelize(newRows)
+//show the values of the parrallized array
+parallelizedRows.collect()
+// make a new dataFrame from the flattened array and schema description
+val newDF = spark.createDataFrame(parallelizedRows, schema)
+//use the union method to append the df with the newDF
+df.union(newDF).where("count = 1").where($"ORIGIN_COUNTRY_NAME" =!= "United States").show() 
+// the different ways to sort a dataFrame
+df.sort("count").show(5)
+df.orderBy("count", "DEST_COUNTRY_NAME").show(5)
+df.orderBy(col("count"), col("DEST_COUNTRY_NAME")).show(5)
+// use the asc or desc functions to sort by ascending or descending (asc_nulls_first or desc_nulls_last are other possibilities)
+import org.apache.spark.sql.functions.{desc, asc_nulls_first}
+df.orderBy(expr("count desc")).show(2)
+df.orderBy(desc("count"), asc_nulls_first("DEST_COUNTRY_NAME")).show(2)
+// it is sometimes advisable to use the sortWithinPartitions
+spark.read.format("json").load("/user/sjf/data/flight_data/json/*-summary.json").sortWithinPartitions("count").show()
+//use the limit methond to show the top ten rows
+df.limit(10).show()
+// show the six largest counts
+df.orderBy(expr("count desc")).limit(6).show()
+// this shows number of partitions accross the cluster
+df.rdd.getNumPartitions 
+// repartition will cause another shuffle to occur
+df.repartition(5)
+// repartition based on a column that would typically be used as an index
+df.repartition(col("DEST_COUNTRY_NAME"))
+// optionally sepcify the number of partitions
+df.repartition(5, col("DEST_COUNTRY_NAME")).coalesce(2)
+// in Scala
+val collectDF = df.limit(10)
+//take flattens your data and requires an integer
+collectDF.take(5)
+// prints out the dataFrame 
+collectDF.show()
+//show only the first five rows
+collectDF.show(5, false)
+//collect rows to the driver - this can crash the driver 
+collectDF.collect()
